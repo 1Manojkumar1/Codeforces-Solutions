78b5dadae44421219995a7abb6176cb206037e42
Manoj <24eg105p52@anurag.edu.in>
2026-08-24 15:15:13 +0530

    236A

---
+++ 800/236A-Boy_or_Girl.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    unordered_set<char> st;
+    for(int i=0;i<s.size();i++){
+        st.insert(s[i]);
+    }
+    if(st.size()%2==1){
+        cout<<"IGNORE HIM!";
+    }else{
+        cout<<"CHAT WITH HER!";
+    }
+    return 0;
+}
