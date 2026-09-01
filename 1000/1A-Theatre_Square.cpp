ebeb52071f365a33a2c3ae0569b2f7d54787491c
Manoj <24eg105p52@anurag.edu.in>
2026-09-01 22:49:52 +0530

    1A

---
+++ 1000/1A-Theatre_Square.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m,a;
+    cin>>n>>m>>a;
+    long long l=n/a,w=m/a;
+    if((n%a)!=0){
+        l++;
+    }
+    if((m%a)!=0){
+        w++;
+    }
+    cout<<l*w;
+    return 0;
+}
