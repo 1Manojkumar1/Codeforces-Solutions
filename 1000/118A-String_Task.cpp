2dd0cee278a5db39fd4cd0a1969a8a36afdbe938
Manoj <24eg105p52@anurag.edu.in>
2026-09-02 22:18:59 +0530

    118A

---
+++ 1000/118A-String_Task.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    for(int i=0;i<s.size();i++){
+        if(s[i]!='A' && s[i]!='O' && s[i]!='Y' && s[i]!='E' && s[i]!='U' && s[i]!='I' && s[i]!='a' && s[i]!='o' && s[i]!='y' && s[i]!='e' && s[i]!='u' && s[i]!='i'){
+            cout<<'.'<<(char)tolower(s[i]);
+        }
+    }
+    return 0;
+}
