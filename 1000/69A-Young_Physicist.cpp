29865206c7d40e80d571109a6c1e24fcbd1c1506
Manoj <24eg105p52@anurag.edu.in>
2026-09-03 23:07:15 +0530

    69A

---
+++ 1000/69A-Young_Physicist.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int x,y,z;
+    int xSum=0,ySum=0,zSum=0;
+    for(int i=0;i<n;i++){
+        cin>>x>>y>>z;
+        xSum+=x;
+        ySum+=y;
+        zSum+=z;
+    }
+    if(xSum==0 && ySum==0 && zSum==0){
+        cout<<"YES";
+    }else{
+        cout<<"NO";
+    }
+    return 0;
+}
