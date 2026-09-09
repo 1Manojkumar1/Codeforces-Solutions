e63abe6b482eca3378da3d8092a9e0ca850bb4e3
Manoj <24eg105p52@anurag.edu.in>
2026-09-09 23:25:53 +0530

    1374B

---
+++ 900/1374B-Multiply_by_2,_divide_by_6.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        int two=0,three=0;
+        while(n%2==0){
+            n/=2;
+            two++;
+        }
+        while(n%3==0){
+            n/=3;
+            three++;
+        }
+        if(n!=1||two>three){
+            cout<<-1<<'\n';
+        }else{
+            cout<<2*three-two<<'\n';
+        }
+    }
+}
